6d3e40ccce1b1b8de5e2d9b9c7075c2037721743
Ramiro Lopez <rlopez@procontsys.com>
2026-03-18 11:24:17 -0500

    Fix home route by removing nested router from App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,5 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import PublicLanding from './pages/PublicLanding';
-import Admin from './pages/Admin';
-import Users from './pages/Users';
-import Dashboard from './pages/Dashboard';
-import Compare from './pages/Compare';
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<PublicLanding />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/admin/users" element={<Users />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/compare" element={<Compare />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <PublicLanding />;
 }
